8470a7eb246ea98a6987fa89657091d624f6ac1d
Pranjul <pranjulsaxena50@gmail.com>
2025-06-19 22:53:29 +0530

    deploy

--- client/src/api/axiosConfig.ts
+++ client/src/api/axiosConfig.ts
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', // base URL of your backend
+  baseURL: 'https://tle-sms-1.onrender.com/api/v1', // base URL of your backend
+  // baseURL: 'http://localhost:5000/api/v1', // base URL of your backend
 });
 
 export default apiClient;
